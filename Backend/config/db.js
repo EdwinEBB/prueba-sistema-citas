8a5b54eccc40c3ae2aca427603c3917a04f457f0
EdwinEBB <edwinelcrak40@gmail.com>
2026-08-21 21:55:36 -0500

    nuevo: iniciando el proyecto con estructura de carpetas en backend y conexión a la base de datos

---
+++ Backend/config/db.js
@@ -0,0 +1,16 @@
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD||'18092003',
+  database: process.env.DB_NAME||'sistema_citas',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+export default pool;
